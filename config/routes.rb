e41184049fdbcd8cf28ec468dba1c487cbe382f5
EC2 Default User <s.marika441@gmail.com>
2021-12-26 14:37:15 +0000

    [Add]複数権限のdevise設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # 管理者
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  # 会員
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "customer/registrations",
+    sessions: "customer/sessions"
+  }
+  
 end
